a78cd64dc0a780a4fad3d8d3675c8b39341a8517
Manuel Rivera <manuel.rivera17788@hotmail.com>
2020-10-20 22:29:52 -0500

    Cambiando api key toekn por valores de la base de datos de producción

--- src/frontend/config/index.js
+++ src/frontend/config/index.js
@@ -1,7 +1,7 @@
 const config = {
   backendUrl: process.env.BACKEND_URL,
-  apiKeyTokenAdmin: process.env.API_KEY_TOKEN_ADMIN || '38ed9b941cca3a940680e6a5d92aa76c4c443adb023e74c2d1a44a5b10582bf8',
-  apiKeyTokenPublic: process.env.API_KEY_TOKEN_PUBLIC || '68c8ccb4ebb48f550257ed00bae69b3f4a18aaf56989fb2e802c1f3a6663ec2d',
+  apiKeyTokenAdmin: process.env.API_KEY_TOKEN_ADMIN,
+  apiKeyTokenPublic: process.env.API_KEY_TOKEN_PUBLIC,
 };
 
 export default config;
