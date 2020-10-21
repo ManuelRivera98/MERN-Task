f61e96c3b614b3d1e5ca230bfc0ade8d6fcdd1ba
Manuel Rivera <manuel.rivera17788@hotmail.com>
2020-10-20 22:22:20 -0500

    Eliminando localhost como url

--- src/frontend/config/index.js
+++ src/frontend/config/index.js
@@ -1,5 +1,5 @@
 const config = {
-  backendUrl: process.env.BACKEND_URL || 'http://localhost:4000',
+  backendUrl: process.env.BACKEND_URL,
   apiKeyTokenAdmin: process.env.API_KEY_TOKEN_ADMIN || '38ed9b941cca3a940680e6a5d92aa76c4c443adb023e74c2d1a44a5b10582bf8',
   apiKeyTokenPublic: process.env.API_KEY_TOKEN_PUBLIC || '68c8ccb4ebb48f550257ed00bae69b3f4a18aaf56989fb2e802c1f3a6663ec2d',
 };
