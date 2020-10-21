569ad76d66ed2fc7f6fc1aafcd54cb6e538d4694
Manuel Rivera <manuel.rivera17788@hotmail.com>
2020-10-20 23:07:12 -0500

    Agregando dominio directamente a la configuración del cliente axios.

--- src/frontend/config/index.js
+++ src/frontend/config/index.js
@@ -1,5 +1,5 @@
 const config = {
-  backendUrl: process.env.BACKEND_URL,
+  backendUrl: 'https://tasks-mern.herokuapp.com',
   apiKeyTokenAdmin: '1B7696D175EEE6D9483C474BFDF41',
   apiKeyTokenPublic: 'D397D1D9CC9BF21C9F895EA2E6316',
 };
